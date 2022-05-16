c390c3e6beed890ed17ac876670167dbd41c3166
GautamBaishya <gautambaishya893@gmail.com>
2022-05-16 14:54:32 +0530

    Second Commit

--- main.js
+++ main.js
@@ -48,36 +48,36 @@ btn.addEventListener('mouseout', (e) => {
     
 });
 
-// const myForm = document.querySelector('#my-form');
-// const nameInput = document.querySelector('#name');
-// const emailInput = document.querySelector('#email');
-// const msg = document.querySelector('.msg');
-// const userList = document.querySelector('#users');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-// myForm.addEventListener('submit', onSubmit);
+myForm.addEventListener('submit', onSubmit);
 
-// function onSubmit(e){
-//     e.preventDefault();
-//     console.log(nameInput.value);
+function onSubmit(e){
+    e.preventDefault();
+    console.log(nameInput.value);
 
-//     if(nameInput.value === '' || emailInput.value === ''){
-//         //alert('Please enter fields');
-//         msg.classList.add('error');
-//         msg.innerHTML = 'Please enter all fields';
+    if(nameInput.value === '' || emailInput.value === ''){
+        //alert('Please enter fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
 
-//         setTimeout(()=> msg.remove(),3000);
-//     } else {
-//         //console.log('Success');
-//         const li = document.createElement('li');
-//         li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        setTimeout(()=> msg.remove(),3000);
+    } else {
+        //console.log('Success');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
 
-//         userList.appendChild(li);
+        userList.appendChild(li);
 
-//         //clear the fields
-//         nameInput.value ='';
-//         emailInput.value = '';
-//     }
-// }
+        //clear the fields
+        nameInput.value ='';
+        emailInput.value = '';
+    }
+}
 
 
 
